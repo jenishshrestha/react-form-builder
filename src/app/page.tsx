a8d9815f9f6496d80837660163ddceef4af09915
jenishshrestha <jenishhrestha@gmail.com>
2024-12-24 22:18:31 +0545

    Form Builder tasks: - [x] set up of shadcdn componenets. - [x] added preview section. - [x] field click functionality, filter functionality with debounce. - [x] json generation on form add. - [x] dependencies import of added fields were generated succesfully.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,50 @@
+"use client";
+
+import { If, Then, Else } from "react-if";
+
+import FieldListings from "@/components/form/field-listing";
+
+//context
+import { useAppContext } from "@/context/AppContextProvider";
+
+// assets
+import EmptyListSvg from "@/assets/oc-thinking.svg";
+import Preview from "@/components/form/preview";
+
+/**
+ * ===============================================================================
+ * Homepage componenet
+ * ===============================================================================
+ * @returns
+ */
 export default function Home() {
+  const { formFields } = useAppContext();
+
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 items-start gap-8 md:px-5 h-full">
-      Div
+    <div className="flex gap-3 w-full px-4 py-20">
+      {/* Left sidebar */}
+      <div className="w-[250px] h-full border-r border-neutral-800 pr-4 overflow-auto">
+        <FieldListings />
+      </div>
+
+      {/* dynamic components */}
+      <If condition={formFields.length > 0}>
+        <Then>
+          <div className="flex w-full h-full overflow-auto">
+            <div className="w-1/2 basis-auto flex-shrink-0 flex-grow-0">
+              {/* Draggable Area */}
+            </div>
+            <div className="w-1/2 basis-auto flex-shrink-0 flex-grow-0">
+              <Preview />
+            </div>
+          </div>
+        </Then>
+        <Else>
+          <div className="flex w-full items-center justify-center">
+            <EmptyListSvg className="h-[1em] text-[251px]" />
+          </div>
+        </Else>
+      </If>
     </div>
   );
 }
